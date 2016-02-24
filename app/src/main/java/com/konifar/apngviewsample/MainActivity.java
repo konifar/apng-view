2c46685d6114a4cddde2b8c4a8b640219d01a8c2
konifar <yahpeycoy0403@gmail.com>
2016-02-25 01:44:24 +0900

    Create sample project base.

--- app/src/main/java/com/konifar/apngviewsample/MainActivity.java
+++ app/src/main/java/com/konifar/apngviewsample/MainActivity.java
@@ -1,31 +1,26 @@
 package com.konifar.apngviewsample;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String REPO_URL = "https://github.com/konifar/apng-view";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -36,9 +31,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_link:
+                AppUtils.showWebPage(REPO_URL, this);
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
