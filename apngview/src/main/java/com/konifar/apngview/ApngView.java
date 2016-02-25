5459a5268c43cd9492dccc83a2a1fb2f5f24a5f3
konifar <yahpeycoy0403@gmail.com>
2016-02-26 03:10:04 +0900

    Add image view

--- apngview/src/main/java/com/konifar/apngview/ApngView.java
+++
@@ -1,5 +0,0 @@
-package com.konifar.apngview;
-
-public class ApngView {
-    private static String TAG = ApngView.class.getSimpleName();
-}
--- app/src/main/java/com/konifar/apngviewsample/MainActivity.java
+++ app/src/main/java/com/konifar/apngviewsample/MainActivity.java
@@ -1,10 +1,16 @@
 package com.konifar.apngviewsample;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +22,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initToolbar();
+
+        initView();
+    }
+
+    private void initView() {
+        final ImageView imgView = (ImageView) findViewById(R.id.img_view);
+        Picasso.with(this)
+                .load("http://ics-web.jp/lab-data/140930_apng/images/elephant_apng_zopfli.png")
+                .into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        imgView.setImageBitmap(bitmap);
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Drawable errorDrawable) {
+
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                    }
+                });
     }
 
     private void initToolbar() {
